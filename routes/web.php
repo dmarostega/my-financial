773c20ca9a9e0d566c9f9ffc47476364c45832bd
Diogo <dmarostega@gmail.com>
2022-09-11 08:55:59 -0300

    refactor: updated index link

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
