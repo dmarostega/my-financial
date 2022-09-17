94dac54c29cf247e8bae68da31830f1c8f326b5a
Diogo <dmarostega@gmail.com>
2022-09-17 10:25:06 -0300

    feat(system): adjust form component

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,4 +1,5 @@
-<form action="{{ $action ?? '#' }}" method="{{ $method ?? 'post' }}">
+@props(['action','method'])
+<form action="{{ $action ?? '#' }}" method="{{ $method ?? 'post' }}"{!! $attributes->merge(['class'=> '']) !!}>
     @csrf
     <div class="flex flex-col">
         {{ $content ?? 'no-content' }}
