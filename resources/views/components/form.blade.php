c24413b3b99b89b0cb6863706fc0b49708ffc559
Diogo <dmarostega@gmail.com>
2022-09-17 17:22:06 -0300

    feat(system): include method

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,7 +1,10 @@
-@props(['action','method'])
-<form action="{{ $action ?? '#' }}" method="{{ $method ?? 'post' }}"{!! $attributes->merge(['class'=> '']) !!}>
+@props(['action','method','get' => false])
+<form action="{{ $action ?? '#' }}" method="{{ ($get ? $get : null) ?? 'post' }}"{!! $attributes->merge(['class'=> '']) !!}>
     @csrf
+    @if(isset($method))
+        @method($method)
+    @endif
     <div class="flex flex-col">
-        {{ $content ?? 'no-content' }}
+        {{ $slot ?? $content ?? 'no-content' }}
     </div>
 </form>
