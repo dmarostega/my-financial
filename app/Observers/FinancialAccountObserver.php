e2032eab34847d7b8036172a3b9cab338a0d307e
Diogo <dmarostega@gmail.com>
2022-09-28 22:14:04 -0300

    feat(financial account): create 'creating' method on Observer with relation

--- app/Observers/FinancialAccountObserver.php
+++ app/Observers/FinancialAccountObserver.php
@@ -2,4 +2,18 @@
 namespace App\Observers;
 
 class FinancialAccountObserver 
-{}
+{
+    /**
+     * 
+     * Handle the financial account "creating" event.
+     * 
+     * @param \App\Models\FinancialAccount
+     * @return void
+     * 
+     */
+
+     public function creating(FinancialAccount $financialAccount)
+     {
+        $finaicialAccount->person_id = auth()->user()->person->id;
+     }
+}
