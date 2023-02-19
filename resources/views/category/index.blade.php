3f5a1db3a34318722490aae71eb25ad56af41456
Diogo <dmarostega@gmail.com>
2023-02-19 10:16:36 -0300

    refactor(category): list, updated layout table with x-blade

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -5,8 +5,8 @@
     <x-button type="button">
         <a class="block mx-auto" href="{{ route('category.create') }}">Novo</a>
     </x-button>
-    <table>
-        <thead>
+    <x-table>
+        <x-slot name="headers">
             <tr>
                 <td>{{ __('name') }}</td>
                 <td>{{ __('description') }}</td>
@@ -14,36 +14,38 @@
                 <td>{{ __('updated at') }}</td>
                 <td>{{ __('deleted at') }}</td>
                 <td>{{ __('actions') }}</td>
-            </tr>            
-        </thead>
-        @foreach ($categories as $item)
-            <tr>
-                <td>
-                    {{ $item->name }}
-                </td>
-                <td>
-                    {{ $item->description }}
-                </td>
-                <td>
-                    {{ $item->created_at }}
-                </td>
-                <td>
-                    {{ $item->updated_at }}
-                </td>
-                <td>
-                    {{ $item->deleted_at }}
-                </td>
-                <td>
-                    <ul>
-                        <li>
-                            <a href="{{ route('category.edit',['id' => $item->id]) }}">{{ __('edit') }}</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('category.delete',['id' => $item->id]) }}">{{ __('delete') }}</a>
-                        </li>
-                    </ul>
-                </td>
-            </tr>            
-        @endforeach
-    </table>    
+            </tr>
+        </x-slot>
+        <x-slot name="body">
+            @foreach ($categories as $item)
+                <tr>
+                    <td>
+                        {{ $item->name }}
+                    </td>
+                    <td>
+                        {{ $item->description }}
+                    </td>
+                    <td>
+                        {{ $item->created_at }}
+                    </td>
+                    <td>
+                        {{ $item->updated_at }}
+                    </td>
+                    <td>
+                        {{ $item->deleted_at }}
+                    </td>
+                    <td>
+                        <ul>
+                            <li>
+                                <a href="{{ route('category.edit',['id' => $item->id]) }}">{{ __('edit') }}</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('category.delete',['id' => $item->id]) }}">{{ __('delete') }}</a>
+                            </li>
+                        </ul>
+                    </td>
+                </tr>            
+            @endforeach
+        </x-slot>
+    </x-table>    
 </x-app-layout>
