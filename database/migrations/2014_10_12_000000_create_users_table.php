670c196c85a9dafabb1ada4d5ac90d52dd3ea293
Diogo <dmarostega@gmail.com>
2023-02-26 15:54:08 -0300

    feat(users): added status column, migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
                 $table->id();
                 $table->string('name')->unique();
                 $table->string('email')->unique();
+                $table->enum('status', ['active','inactive'])->default('active');
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');            
                 $table->rememberToken();
