a835d0775712d33ef5514b40bb656cfb7fdc4288
Diogo <dmarostega@gmail.com>
2023-02-26 16:41:44 -0300

    feat(bill): created migration

---
+++ database/migrations/2023_02_26_191504_create_bills_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBiginteger('contract_id')->nullable();
+            $table->string('title',255);
+            $table->double('value');
+            $table->enum('type',['to_pay','to_receive']);
+            $table->enum('frequency',['daily', 'monthly', 'yearly'])->default('monthly');
+            $table->enum('status', [ 'active','inactive'])->default('active');            
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table->foreign('contract_id')->references('id')->on('contracts');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
