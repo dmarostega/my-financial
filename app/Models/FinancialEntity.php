edd5c6f7a7d2c4f3832e1d0b21bbe4140fa9c69d
Diogo <dmarostega@gmail.com>
2023-02-27 06:46:18 -0300

    feat(financial entity): added HandleUser Trait

--- app/Models/FinancialEntity.php
+++ app/Models/FinancialEntity.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\HandleUser;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FinancialEntity extends Model
 {
+    use HandelUser;
+    
     use SoftDeletes;
 
     protected $table = 'financial_entities';
