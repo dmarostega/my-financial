d1e3acd785661f16c587a44a730858ef58eb9db1
Diogo <dmarostega@gmail.com>
2023-02-27 06:45:57 -0300

    feat(financil account): added HandleUser Trait

--- app/Models/FinancialAccount.php
+++ app/Models/FinancialAccount.php
@@ -1,11 +1,14 @@
 <?php 
 namespace App\Models;
 
+use App\Traits\HandleUser;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FinancialAccount extends Model 
 {
+    use HandleUser;
+
     use SoftDeletes;
 
     protected $table = 'financial_accounts';
