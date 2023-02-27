7cab89d33b72df56159457fb4622d53190b35b3a
Diogo <dmarostega@gmail.com>
2023-02-27 06:30:58 -0300

    feat(payment type): added HandleUser Trait

--- app/Models/PaymentType.php
+++ app/Models/PaymentType.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HandleUser;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PaymentType extends Model
 {
+    use HandleUser;
     use SoftDeletes;
     
     protected $table = 'payment_types';
@@ -15,5 +17,4 @@ class PaymentType extends Model
         'name',
         'description'
     ];
-
 }
