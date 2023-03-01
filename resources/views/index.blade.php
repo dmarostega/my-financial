d5c6885d3ba10b7c5b42a997eb21d4e73d5cfbbb
Diogo <dmarostega@gmail.com>
2023-03-01 05:05:58 -0300

    refactor(index): corrected links

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,27 @@
 <x-app-layout>   
     <x-flex-box>
-      <x-system-icon :link="__(route('transactions'))">
+      <x-system-icon :link="route('transactions')">
         {{ __('Transações') }}
       </x-system-icon>
-      <x-system-icon :link="__(route('users'))">
+      <x-system-icon :link="route('users')">
         {{ __('Usuários') }}      
       </x-system-icon>
-      <x-system-icon :link="__(route('categories'))">
+      <x-system-icon :link="route('categories')">
         {{ __('Categorias') }}      
       </x-system-icon>
-      <x-system-icon :link="__(route('contracts'))">
+      <x-system-icon :link="route('contracts')">
         {{ __('Contratos') }}
       </x-system-icon>
-      <x-system-icon :link="__(route('financial_entities'))">
+      <x-system-icon :link="route('financial_entities')">
         {{ __('Agências') }} {{ __('bancárias') }}
       </x-system-icon>
-      <x-system-icon :link="__(route('financial_accounts'))">
+      <x-system-icon :link="route('financial_accounts')">
         {{ __('Contas') }} {{ __('Bancárias') }}
       </x-system-icon>
-      <x-system-icon :link="__(route('cards'))">
+      <x-system-icon :link="route('cards')">
          {{ __('Cartões') }} 
       </x-system-icon>
-      <x-system-icon :link="__(route('payment_types'))">
+      <x-system-icon :link="route('payment_types')">
         {{ __('Tipos') }} {{ __('de') }} {{ __('pagamento') }}
       </x-system-icon>
     </x-flex-box>      
