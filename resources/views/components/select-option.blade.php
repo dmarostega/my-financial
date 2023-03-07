18526fd64fc255051773a684cd412613e07c0699
Diogo <dmarostega@gmail.com>
2023-03-07 05:40:06 -0300

    fix(system): select option component, reafctor default value

--- resources/views/components/select-option.blade.php
+++ resources/views/components/select-option.blade.php
@@ -1,4 +1,4 @@
-@props(['value' => '#', 'isSelected' => false])
+@props(['value' => '', 'isSelected' => false])
 
 <option value="{{ $value }}" {!! $attributes->merge(['class'=>'hidden space-x-8 sm:-my-px sm:ml-10 sm:flex']) !!} {{ $isSelected == 1 ? 'selected' : ''}}>
     {{ $slot }}
