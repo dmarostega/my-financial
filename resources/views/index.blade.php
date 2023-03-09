fc75a5bda37a5e4a8b43ace6babf5ed0e830c640
Diogo <dmarostega@gmail.com>
2023-03-09 19:57:13 -0300

    feat(index): adjust layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,40 @@
 <x-app-layout>  
+  <x-flex-box>
     <div>
-      <x-link href="{{ route('check_bills') }}">
-        {{ __('Check bills') }}
-      </x-link>
-      <x-link href="{{ route('check_transactions') }}">
-        {{ __('Check transactions') }}
-      </x-link>
-    </div> 
-    <x-flex-box>
-      <x-system-icon :link="route('transactions')">
-        {{ __('Transações') }}
-      </x-system-icon>
-      <x-system-icon :link="route('bills')">
-        {{ __('Contas') }}
-      </x-system-icon>      
-      <x-system-icon :link="route('users')">
-        {{ __('Usuários') }}      
-      </x-system-icon>
-      <x-system-icon :link="route('categories')">
-        {{ __('Categorias') }}      
-      </x-system-icon>
-      <x-system-icon :link="route('contracts')">
-        {{ __('Contratos') }}
-      </x-system-icon>
-      <x-system-icon :link="route('financial_entities')">
-        {{ __('Agências') }} {{ __('bancárias') }}
-      </x-system-icon>
-      <x-system-icon :link="route('financial_accounts')">
-        {{ __('Contas') }} {{ __('Bancárias') }}
-      </x-system-icon>
-      <x-system-icon :link="route('cards')">
-         {{ __('Cartões') }} 
-      </x-system-icon>
-      <x-system-icon :link="route('payment_types')">
-        {{ __('Tipos') }} {{ __('de') }} {{ __('pagamento') }}
-      </x-system-icon>
+        <div>
+          <x-link href="{{ route('check_bills') }}">
+            {{ __('Check bills') }}
+          </x-link>
+          <x-link href="{{ route('check_transactions') }}">
+            {{ __('Check transactions') }}
+          </x-link>
+        </div> 
+        <x-system-icon :link="route('transactions')">
+          {{ __('Transações') }}
+        </x-system-icon>
+        <x-system-icon :link="route('bills')">
+          {{ __('Contas') }}
+        </x-system-icon>
+      </div>
+      <div>
+        <x-system-icon :link="route('contracts')">
+          {{ __('Contratos') }}
+        </x-system-icon>
+        <x-system-icon :link="route('financial_entities')">
+          {{ __('Agências') }} {{ __('bancárias') }}
+        </x-system-icon>
+        <x-system-icon :link="route('financial_accounts')">
+          {{ __('Contas') }} {{ __('Bancárias') }}
+        </x-system-icon>
+        <x-system-icon :link="route('cards')">
+          {{ __('Cartões') }} 
+        </x-system-icon>
+        <x-system-icon :link="route('payment_types')">
+          {{ __('Tipos') }} {{ __('de') }} {{ __('pagamento') }}
+        </x-system-icon>
+        <x-system-icon :link="route('users')">
+          {{ __('Usuários') }}      
+        </x-system-icon>
+      </div>
     </x-flex-box> 
 </x-app-layout>
