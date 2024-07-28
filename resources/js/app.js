697af5cea5bf6adb39c44e8bca6fb21d62ea5c62
dmarostega@gmail.com <dmarostega@gmail.com>
2024-07-28 17:55:26 -0300

    feat(system): implemented maskMoney, remove plugin mask alpineJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,40 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import mask from '@alpinejs/mask'
+// import mask from '@alpinejs/mask'
  
-Alpine.plugin(mask)
+// Alpine.plugin(mask)
 
 window.Alpine = Alpine;
+
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('moneyMask', () => ({
+        value: '',
+        applyMask() {
+            this.value = maskMoney(this.value);
+        }
+    }));
+});
+
+
+const maskMoney = function (input) {
+    // Remove tudo que não é dígito
+    let v = input.replace(/\D/g, '');
+    
+    // Adiciona os zeros no início caso o valor seja menor que 3 dígitos (para garantir pelo menos R$ 0,00)
+    v = (v / 100).toFixed(2) + '';
+    
+    // Substitui ponto por vírgula
+    v = v.replace('.', ',');
+    
+    // Adiciona o separador de milhar
+    v = v.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    
+    // Adiciona o prefixo "R$ "
+    return 'R$ ' + v;
+}
+
 
 Alpine.start();
 
